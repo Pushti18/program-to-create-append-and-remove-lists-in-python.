c640dd9bf87a2727d9673f479abe6a9fda9d038b
Pushti Depani <77913618+Pushti18@users.noreply.github.com>
2022-02-25 21:03:41 +0530

    Add files via upload

---
+++ 05.py
@@ -0,0 +1,9 @@
+//05. Write a python program to create, append and remove lists in python.
+p = [1, 'a' ,3.5 ]
+print(p)
+a = ['apple', 'banana', 'cherry']
+a.append('banana')
+print(a)
+a = ['apple', 'banana', 'cherry']
+a.remove('banana')
+print(a)
